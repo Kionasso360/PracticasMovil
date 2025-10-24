e0e6c8b8b8f78171d57a98b80df76334808a011d
Kionasso360 <ec1503526@gmail.com>
2025-10-23 19:20:28 -0600

    prueba

---
+++ app/src/main/java/screen/banderaMexico.kt
@@ -0,0 +1,26 @@
+package screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BanderaMexico(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.fillMaxSize()) {
+        Box(modifier = Modifier.weight(1f).fillMaxHeight().background(Color(0xFF006847)))
+        Box(modifier = Modifier.weight(1f).fillMaxHeight().background(Color.White))
+        Box(modifier = Modifier.weight(1f).fillMaxHeight().background(Color(0xFFCE1126)))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMexico() {
+    BanderaMexico()
+}
